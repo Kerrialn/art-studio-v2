f666b8b0970c171c40a26fdba1e7bcfd5628ac5d
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-02-04 13:57:02 +0100

    fix readme

---
+++ src/components/Exhibitions.vue
@@ -0,0 +1,26 @@
+<template>
+		<div class="container">
+				<section class="hero">
+						<div class="hero-body">
+								<div class="container">
+										<h1 class="title">
+												Coming soon
+										</h1>
+										<h2 class="subtitle">
+												Please check back later for this section
+										</h2>
+								</div>
+						</div>
+				</section>
+		</div>
+</template>
+
+<script>
+    export default {
+        name: "Exhibitions"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,38 @@
+<template>
+		<div class="container">
+				<div class="columns is-multiline is-mobile">
+
+								<div v-for="art in artworks" :key="art.id" class="column is-one-quarter">
+										<figure class="image w-100">
+												<artwork :obj="art"></artwork>
+										</figure>
+								</div>
+
+				</div>
+		</div>
+</template>
+
+<script>
+    import Artwork from "./Artwork";
+    export default {
+        name: "Gallery",
+        components: {Artwork},
+        data() {
+            return {
+                artworks: [
+                    {id: 1, name: 'A Modern World', image: 'A modern world.jpg', description: 'different thing'},
+                    {id: 2, name: 'After Glow', image: 'Afterglow-web.jpg', description: 'something about this art peice'},
+                    {id: 3, name: 'Biology', image: 'Biology 1.jpg', description: 'something about this art peice'},
+                    {id: 4, name: 'Bodyclock', image: 'Bodyclock.jpg', description: 'something about this art peice'},
+                    {id: 5, name: 'So this is where it went down', image: 'So this is where it went down.jpg', description: 'something about this art peice'},
+                ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+.w-100{
+		width: 100%;
+}
+</style>
